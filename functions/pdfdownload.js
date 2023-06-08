e52198bdef70e756e295e016fb72649180fc66b6
michael levy <michael.levy@abbvie.com>
2023-06-08 15:34:18 -0400

    [chore] got pdf working.

--- functions/pdfdownload.js
+++ functions/pdfdownload.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
-const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
 
-const COL_HEIGHT = 30;
-const COLUMN_WIDTHS = [50, 80, 300];
+const { PDFDocument, rgb, encodeToBase64 } = require('pdf-lib');
+const fontkit = require('@pdf-lib/fontkit');
+
+const COL_HEIGHT = 27;
+const COLUMN_WIDTHS = [40, 85, 365];
 const MAX_PER_PAGE = 18;
-const COLOR_LIGHT_GRAY = rgb(0.960, 0.960, 0.960);
-const COLOR_BLACK = rgb(0, 0, 0);
-const COLOR_WHITE = rgb(1, 1, 1);
-const FONT_SIZE = 15;
+const COLOR_TEXT = rgb(0, 0, 0);
+const COLOR_CELL = rgb(1, 1, 1);
+const COLOR_BORDER = rgb(0.494, 0.494, 0.494);
+const FONT_SIZE = 15.5;
 
 const statusCode = {
 	success: 200,
@@ -16,6 +18,32 @@ const statusCode = {
 	methodNotSupported: 405,
 	missingData: 422
 };
+
+function* chunks(arr, n) {
+  for (let i = 0; i < arr.length; i += n) {
+    yield arr.slice(i, i + n);
+  }
+}
+
+function organizeData(arr) {
+	return chunks(arr.map((product, idx) => ({ 
+		id: `row-${idx}`,
+		columns: [
+		{
+				id: `row-${idx}-col-0`,
+				value: product.Quantity,
+			},
+			{
+				id: `row-${idx}-col-1`,
+				value: product.ItemCode,
+			},
+			{
+				id: `row-${idx}-col-2`,
+				value: product.OrderDescription,
+			}
+		]
+	})), MAX_PER_PAGE);
+}
 
 exports.handler = async function (event, context, callback) {
 	let data;
@@ -34,84 +62,78 @@ exports.handler = async function (event, context, callback) {
   }
 
 	const { _answers: { OrderDetails } } = data;
-	const contents = await fs.readFileSync(require.resolve('./PDF-Template.pdf'));
-  const loadedPdf = await PDFDocument.load(contents);
-  const pdfDoc = await PDFDocument.create();
-	const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-  const fontBold = await pdfDoc.embedFont(StandardFonts.TimesRomanBold);
-	const [loadedPdfFirstPage, loadedPdfSecondPage] = await pdfDoc.copyPages(loadedPdf, [0, 1]);
+	const totalProducts = OrderDetails.length;
+	const pagesData = [...organizeData(OrderDetails)];
 	const table = [];
 
-	OrderDetails.map((product, idx) => {
-		table.push({
-			id: `row-${idx}`,
-			page: idx < MAX_PER_PAGE ? 0 : 1,
-			columns: [
-			{
-					id: `row-${idx}-col-0`,
-					value: product.Quantity,
-				},
-				{
-					id: `row-${idx}-col-1`,
-					value: product.ItemCode,
-				},
-				{
-					id: `row-${idx}-col-2`,
-					value: product.OrderDescription,
-				}
-			]
-		});
-	});
+	const myriadProRegular = await fs.readFileSync(require.resolve('./embedFonts/MyriadPro-Regular.ttf'));
+	const myriadProBold = await fs.readFileSync(require.resolve('./embedFonts/MyriadPro-Bold.ttf'));
+	const contents = await fs.readFileSync(require.resolve('./PDF-Template.pdf'));
+
+  const loadedPdf = await PDFDocument.load(contents);
+  const pdfDoc = await PDFDocument.create();
+
+	pdfDoc.registerFontkit(fontkit);
+
+	const font = await pdfDoc.embedFont(myriadProRegular); 
+  const fontBold = await pdfDoc.embedFont(myriadProBold); 
+	const [loadedPdfFirstPage, loadedPdfSecondPage] = await pdfDoc.copyPages(loadedPdf, [0, 1]);	
+
+	console.log(JSON.stringify(pagesData))
 
 	// If only 18 results, need to show page with footer, 2nd page in template.
-	if (OrderDetails.length <= MAX_PER_PAGE) {
+	if (totalProducts <= MAX_PER_PAGE) {
 		pdfDoc.addPage(loadedPdfSecondPage);
 	} else {
 		pdfDoc.addPage(loadedPdfFirstPage);
-		pdfDoc.addPage(loadedPdfSecondPage);		
+		pdfDoc.addPage(loadedPdfSecondPage);	
 	}
 
-	// const pages = pdfDoc.getPages();
+	const pages = pdfDoc.getPages();
 
-	// pages.forEach((page, pageIdx) => {
-	// 	const { height } = page.getSize();
-	// 	const startYPos = pageIdx === 1 ? height - 1200 : height - 800;
+	pages.forEach((page, pageIdx) => {
+		const { height } = page.getSize();
+		let pageRows = pagesData[pageIdx];
+		const startYPos = height - 600;
 
-	// 	table.forEach((row, rowIdx) => {
-	// 		if (row.page === pageIdx) {
-	// 			row.columns.forEach((col, colIdx) => {
-	// 				const colWidth = COLUMN_WIDTHS[colIdx];
-	// 				let xPos = COL_HEIGHT;
+		console.log(pageIdx, pageRows, pageRows[0].columns[1])
 
-	// 				if (colIdx === 1) {
-	// 					xPos += COLUMN_WIDTHS[0]
-	// 				}
+		pageRows.forEach((row, rowIdx) => {
+			row.columns.forEach((col, colIdx) => {
+				
+				const colWidth = COLUMN_WIDTHS[colIdx];
+				let xPos = 60;
 
-	// 				if (colIdx === 2) {
-	// 					xPos += COLUMN_WIDTHS[0] + COLUMN_WIDTHS[1];
-	// 				}
+				if (colIdx === 1) {
+					xPos += COLUMN_WIDTHS[0]
+				}
 
-	// 				page.drawRectangle({
-	// 					width: colWidth,
-	// 					height: COL_HEIGHT,
-	// 					borderWidth: 1,
-	// 					borderColor: COLOR_BLACK,
-	// 					color: COLOR_WHITE, 
-	// 					x: xPos,
-	// 					y: height - (startYPos + (COL_HEIGHT * rowIdx)),
-	// 				});
+				if (colIdx === 2) {
+					xPos += COLUMN_WIDTHS[0] + COLUMN_WIDTHS[1];
+				}
 
-	// 				page.drawText(`${col.value}, ${rowIdx}.${colIdx}`, {
-	// 					x: xPos + 10,
-	// 					y: height - (startYPos + (COL_HEIGHT * rowIdx)) + 15,
-	// 					size: FONT_SIZE,
-	// 					font: colIdx === 1 ? fontBold : font,
-	// 					color: COLOR_BLACK,
-	// 				})
-	// 			})
-	// 		}
-	// 	})
-	// });
+				page.drawRectangle({
+					width: colWidth,
+					height: COL_HEIGHT,
+					borderWidth: 1,
+					borderColor: COLOR_BORDER,
+					color: COLOR_CELL, 
+					x: xPos,
+					y: height - (startYPos + (COL_HEIGHT * rowIdx)),
+				});
+
+				page.drawText(col.value, {
+					x: xPos + 6,
+					y: height - (startYPos + (COL_HEIGHT * rowIdx)) + 8,
+					size: FONT_SIZE,
+					font: colIdx === 1 ? fontBold : font,
+					color: COLOR_TEXT,
+				})
+
+				pageRows = null;
+			})
+		})
+	});
 
 	const pdfBytes = await pdfDoc.save();
 	
@@ -119,10 +141,11 @@ exports.handler = async function (event, context, callback) {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/pdf",
-			'Access-Control-Allow-Methods': 'POST'
+			'Access-Control-Allow-Methods': 'POST',
+			"content-disposition": "attachment;filename=\"Quiz_Results.pdf\"",
     },
     isBase64Encoded: true,
     statusCode: statusCode.success,
-    body: Buffer.from(pdfBytes).toString('base64'),
+    body: encodeToBase64(pdfBytes),
   });
 };  
